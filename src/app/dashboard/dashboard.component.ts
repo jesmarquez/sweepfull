0038c33e55aa4826f634433a48f91432dad2e6ed
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2024-08-10 12:54:10 -0400

    Add routerlink to places

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
 
 import { Component, OnInit } from "@angular/core";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive]
 })
 export class DashboardComponent implements OnInit {
   ngOnInit(): void {
