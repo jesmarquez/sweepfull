4e060108b8c61a96ad74ffbcde3dd358068777e3
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2024-08-10 14:51:11 -0400

    Add html places

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-places',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './places.component.html',
-  styleUrl: './places.component.css'
+  styleUrl: './places.component.css',
 })
 export class PlacesComponent {
+  mode: string = "show";
+  @ViewChild('placeName') placeNameSearchInputRef!: ElementRef;
+
+
+  onSearch() {
+    const placeName = this.placeNameSearchInputRef.nativeElement.value;
+    console.log('search:' + placeName);
+  }
+  onClickAddPlace() {
+    console.log('add place');
+    this.mode = "edit";
+
+  }
 
 }
